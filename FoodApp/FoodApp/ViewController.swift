372689f1835e17f9f8b621351d57b37ab9796e37
yuyadomae <y.domae@leihauoli.com>
2019-02-28 18:31:13 +0900

    実装途中

--- FoodApp/FoodApp/ViewController.swift
+++ FoodApp/FoodApp/ViewController.swift
@@ -8,22 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate {
     
     //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         nameTextField.delegate = self
 
-        var monster = Monster(name: "勇者", level: 10)
-        var slime = Slime(name: "スライム", level: 5)
-        monster.attackMonster(enemy: slime)
-        slime.attackMonster(enemy: monster)
-        slime.escapeFromMonster(enemy: monster)
+//        var monster = Monster(name: "勇者", level: 10)
+//        var slime = Slime(name: "スライム", level: 5)
+//        monster.attackMonster(enemy: slime)
+//        slime.attackMonster(enemy: monster)
+//        slime.escapeFromMonster(enemy: monster)
         
 
     }
@@ -42,35 +43,44 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func setDefaultLabelText(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
-}
-
-//Monsterクラスの定義
-class Monster {
-    var name: String
-    var level: Int
-    var hp: Int
     
-    init(name: String, level: Int) {
-        self.name = name
-        self.level = level
-        self.hp = 1
-    }
-    
-    func attackMonster(enemy: Monster) {
-        print("\(self.name)は\(enemy.name)を攻撃した。")
+    @IBAction func selectImagePhotoLibrary(_ sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .photoLibrary
+        imagePickerController.delegate = self as! UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        present(imagePickerController, animated: true, completion: nil)
     }
 }
 
-//スライムクラスの定義
-class Slime: Monster {
-    func escapeFromMonster(enemy: Monster) {
-        print("\(self.name)は\(enemy.name)から逃げた。")
-    }
-    
-    override func attackMonster(enemy: Monster) {
-        print("\(self.name)は\(enemy.name)から全てを奪った。")
-    }
-}
+////Monsterクラスの定義
+//class Monster {
+//    var name: String
+//    var level: Int
+//    var hp: Int
+//
+//    init(name: String, level: Int) {
+//        self.name = name
+//        self.level = level
+//        self.hp = 1
+//    }
+//
+//    func attackMonster(enemy: Monster) {
+//        print("\(self.name)は\(enemy.name)を攻撃した。")
+//    }
+//}
+//
+////スライムクラスの定義
+//class Slime: Monster {
+//    func escapeFromMonster(enemy: Monster) {
+//        print("\(self.name)は\(enemy.name)から逃げた。")
+//    }
+//
+//    override func attackMonster(enemy: Monster) {
+//        print("\(self.name)は\(enemy.name)から全てを奪った。")
+//    }
+//}
 
 
 
